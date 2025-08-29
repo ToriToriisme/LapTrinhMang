6fb33268aa0c7a7286ec6094d8e21f74042784cb
Nguyễn Thị Yến Vy <vynty0829@ut.edu.vn>
2025-08-29 07:24:50 +0700

    Add files via upload

---
+++ IP/Server/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        UdpClient client = new UdpClient();
+        string serverIP = "127.0.0.1";
+        int port = 13000;
+
+        Console.WriteLine("nhap ten mien:");
+
+        while (true)
+        {
+            Console.Write("> ");
+            string domain = Console.ReadLine();
+            if (domain.ToLower() == "exit") break;
+
+            byte[] data = Encoding.UTF8.GetBytes(domain);
+            client.Send(data, data.Length, serverIP, port);
+
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] recvData = client.Receive(ref remoteEP);
+            string result = Encoding.UTF8.GetString(recvData);
+
+            Console.WriteLine("Ketqua IP:");
+            Console.WriteLine(result);
+        }
+
+        client.Close();
+    }
+}
---
+++ IP/Server/Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        UdpClient server = new UdpClient(13000);
+        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+        Console.WriteLine("Server đang chay...");
+
+        while (true)
+        {
+            byte[] data = server.Receive(ref remoteEP);
+            string domain = Encoding.UTF8.GetString(data);
+            Console.WriteLine($"nhan ten mien: {domain}");
+
+            string response = "";
+
+            try
+            {
+                IPHostEntry entry = Dns.GetHostEntry(domain);
+                foreach (IPAddress ip in entry.AddressList)
+                {
+                    response += ip.ToString() + "\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                response = "loi phan giai: " + ex.Message;
+            }
+
+            byte[] reply = Encoding.UTF8.GetBytes(response);
+            server.Send(reply, reply.Length, remoteEP);
+        }
+    }
+}
